30b52a54837adc17318eba208ae8ffff6ccc9e1e
Владимир Кондюрин <v.kondyurin@mail.ru>
2021-03-15 09:59:35 +0700

    123

--- avtorizasiya/avtorizasiya.cpp
+++ avtorizasiya/avtorizasiya.cpp
@@ -25,6 +25,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
 
+    
     // TODO: Разместите код здесь.
 
     // Инициализация глобальных строк
